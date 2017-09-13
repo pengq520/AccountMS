4161015024d5667974b5781e8d88fe0dcbe5adab
ios26 <123456>
2017-09-13 15:50:00 +0800

    更新

--- app/src/main/java/com/example/ios27/accountms/model/Tb_flag.java
+++ app/src/main/java/com/example/ios27/accountms/model/Tb_flag.java
@@ -4,35 +4,35 @@ package com.example.ios27.accountms.model;
  * Created by ios26 on 2017/9/13.
  */
 
-public  class  Tb_flag //便签信息实体类
+public class Tb_flag //便签信息实体类
 {
-    private  int _id; //存储便签编号
-    private  String flag; //存储便签信息
+    private int _id; //存储便签编号
+    private String flag; //存储便签信息
 
-    public  Tb_flag () { //默认构造函数
-        super ();
+    public Tb_flag(){ //默认构造函数
+        super();
     }
 
     //定义有参构造函数，用来初始化便签信息实体类中的各个字段
-    public  Tb_flag ( int  id , String  flag ) {
-        super ();
-        this . _id = id; //为便签号赋值
-        this . flag = flag; //为便签信息赋值
+    public Tb_flag(int id,String flag){
+        super();
+        this._id=id; //为便签号赋值
+        this.flag=flag; //为便签信息赋值
     }
 
-    public  int  getid () { //设置便签编号的可读属性
+    public int getid(){ //设置便签编号的可读属性
         return _id;
     }
 
-    public  void  setid ( int  id ) { //设置便签编号的可写属性
-        this . _id = id;
+    public void setid(int id){ //设置便签编号的可写属性
+        this._id=id;
     }
 
-    public  String  getFlag () { //设置便签信息的可读属性
+    public String getFlag(){ //设置便签信息的可读属性
         return flag;
     }
 
-    public  void  setFlag ( String  flag ) { //设置便签信息的可写属性
-        this . flag = flag;
+    public void setFlag(String flag){ //设置便签信息的可写属性
+        this.flag=flag;
     }
 }
